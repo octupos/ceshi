4df799a0e0e2c773ded4fe25a71a770a25b94275
章鱼 <16118707@qq.com>
2016-03-17 09:39:41 +0800

    测试 第一次提交

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by 章鱼 on 16/3/17.
+//  Copyright © 2016年 章鱼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
